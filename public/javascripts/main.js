f3c39b48e428fcd189dc677425df540585a2724a
Dennis Xiao <dennisboys@gmail.com>
2017-01-13 23:42:38 +1100

    fine-tuned some visual effects

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -28,30 +28,34 @@ function check_if_in_view() {
 
 
 $(function() {
+
+    // above-the-fold typing animation
     $("#introduction").typed({
         strings: [" Hi, I'm Dennis Xiao. <br> <span style='font-size:30px;'> I'm a full-stack web developer living in Australia.<span> " ],
         typeSpeed: -5,
         showCursor: false
     });
 
-    // fadeIntext
- setTimeout(function(){
-        $(".message").css('visibility','visible').hide().fadeIn(1500)
-  },1000);
+    // fade in message
+    setTimeout(function(){
+        $(".message").css('visibility', 'visible').hide().fadeIn(1500)
+    }, 1000);
 
-// smooth scroll
-  $('.page-scroll').click(function(){
-    //Toggle Class
-    $(".active1").removeClass("active1");
-    $(this).closest('li').addClass("active1");
-    var theClass = $(this).attr("class");
-    $('.'+theClass).parent('li').addClass('active1');
-    //Animate
-    $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 0
-    }, 500);
-    return false;
-  });
+    // smooth scroll
+    $('.page-scroll').click(function(){
+        // Toggle Class
+        $(".active1").removeClass("active1");
+        $(this).closest('li').addClass("active1");
+        let theClass = $(this).attr("class");
+        $('.' + theClass).parent('li').addClass('active1');
+        // Animate
+        $('html, body').stop().animate({
+            scrollTop: $( $(this).attr('href') ).offset().top - 0
+        }, 500);
+
+        return false;
+    });
+
   //scroll to top (not set yet)
   $('.scrollTop a').scrollTop();
 
